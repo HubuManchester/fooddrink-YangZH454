a6c612780753d4de07165b0aa91ba6c41d2210f5
“YangZH454” <2926384707@qq.com>
2026-05-30 19:45:37 +0800

    Commit 1: Setup NutriSnap project structure, core models, and basic shell navigation.

---
+++ NutriSnap/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NutriSnap
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NutriSnap/Models/FoodItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutriSnap.Models
+{
+    public class FoodItem
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("N");
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public int Calories { get; set; }
+        public string AccessibleSummary => $"{Name}. Category: {Category}. Calories: {Calories} kcal.";
+    }
+}
